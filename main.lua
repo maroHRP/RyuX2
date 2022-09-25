4245ed7d030750faf0ff87272e1a298bba86dc17
maroHRP <maro.rukavina@gmail.com>
2022-09-25 17:15:06 +0200

    test za 5 minuti

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 8750997647 then
+if game.PlaceId == testzaminuticu then
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
     local Window = OrionLib:MakeWindow({Name = "Ryu X", HidePremium = true, IntroText = "Ryu X", SaveConfig = true, ConfigFolder = "OrionTest"})
     
